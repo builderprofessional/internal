440f40bab3b2dad5951aa98252d614a21b231ed4
Tony Vance <tony.vance@latentcodex.com>
2016-01-31 01:01:59 +0000

    adding proper definitions to the marketing bundle

--- src/Internal/MarketingBundle/InternalMarketingBundle.php
+++ src/Internal/MarketingBundle/InternalMarketingBundle.php
@@ -1,9 +1,17 @@
 <?php
-
 namespace Internal\MarketingBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class InternalMarketingBundle extends Bundle
 {
+  /**
+   * this will allow the reverse command to tell which tables belong to this bundle
+   */
+  public static $defaultTablePrefix = 'marketing';
+
+  /**
+   * this will define which bundle tables without prefixes should get added to
+   */
+  public static $defaultBundle = false;
 }
