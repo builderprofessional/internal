5e9ef05cd5a402c9743c4928bdc37464031b6967
Robert Flach <robert@point-of-rental.com>
2016-02-13 13:24:13 -0600

    making the contact us form be enginized.

---
+++ web/app/internal/views/contact/contactUs.js
@@ -0,0 +1,48 @@
+(function ()
+{
+  internalApp.directive("viewContactUs", ['$http', '$location', 'PropelSOAService', '$stateParams', 'engState',
+                                                    'engValidation', '$q', 'engAlert','$modal', contactUs]);
+  function contactUs($http, $location, PropelSOAService, $stateParams, engState, engValidation, $q, engAlert, $modal)
+  {
+    return {
+      restrict: "A",
+      scope: {},
+      templateUrl: "/app/internal/views/contact/partial.html",
+      controller: ['$scope',
+        function ($scope)
+        {
+          $scope.ContactUs = {};
+          var qRules = engValidation.getRuleset('contactus');
+          $q.all([qRules]).then(function ()
+            {
+              $scope.validator = engValidation.getValidator('contactus');
+              $scope.validator.watch($scope, $scope.ContactUs);
+            });
+          $scope.contactUs = function()
+          {
+            $scope.contactUsModal = $modal(
+              {
+                title:'Get in Touch with Builder Professional',
+                contentTemplate: 'contact-us-modal.html',
+                scope:$scope
+              });
+          };
+
+          $scope.cancel = function(){
+            $scope.contactUsModal.hide();
+          };
+          $scope.save = function ()
+          {
+            $scope.validator.isValid().then(function(result){
+              // Clear out the Braintree model to ensure that we are keeping the values for as short a time as possible
+              $http.post(env_url+'/public/contactus'+env_postfix,$scope.ContactUs);
+              $scope.contactUsModal.hide();
+            },function(result){
+              engAlert.alert('contactus_error','Please ensure all information is correct before saving.', 'contactError');
+            });
+          };
+        }
+      ]
+    };
+  }
+})();
